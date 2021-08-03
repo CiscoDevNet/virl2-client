450a25d77ff6973fe62be0b3d4c19f6eae0b35a5
Samuel Elias <samelias@cisco.com>
2021-08-03 10:06:31 +0200

    SIMPLE-3574 Integration tests for Licensing feature - transport

--- tests/test_licensing.py
+++ tests/test_licensing.py
@@ -21,6 +21,9 @@
 import pytest
 import requests.exceptions
 
+from datetime import datetime
+from time import sleep
+
 from virl2_client import ClientLibrary
 
 pytestmark = [pytest.mark.integration, pytest.mark.nomock]
@@ -46,6 +49,8 @@ def cleanup_registration(
     Register it back at the end of the test module.
     """
     ensure_unregistered(client_library_session)
+    client_library_session.licensing.set_default_transport()
+    ensure_no_certificate(client_library_session)
     yield
     client_library_session.licensing.set_transport(ssms=alpha_ssms_url)
     client_library_session.licensing.install_certificate(cert=alpha_dev_ca)
@@ -115,3 +120,116 @@ def test_certificate_invalid(
     with pytest.raises(requests.exceptions.HTTPError) as exc:
         cl.licensing.install_certificate(certificate_invalid)
     exc.match("400 Client Error: Bad Request for url")
+
+
+def test_set_transport(client_library_session: ClientLibrary, alpha_ssms_url):
+    cl = client_library_session
+    """Configure valid transport settings."""
+
+    cl.licensing.set_transport(None)
+    transport = cl.licensing.status()["transport"]
+    assert transport["ssms"] == transport["default_ssms"]
+    assert transport["proxy"]["server"] is None
+    assert transport["proxy"]["port"] is None
+
+    cl.licensing.set_transport(alpha_ssms_url)
+    transport = cl.licensing.status()["transport"]
+    assert transport["ssms"] == alpha_ssms_url
+    assert transport["proxy"]["server"] is None
+    assert transport["proxy"]["port"] is None
+
+    cl.licensing.set_transport(
+        alpha_ssms_url, proxy_server="127.0.0.1", proxy_port=49152
+    )
+    transport = cl.licensing.status()["transport"]
+    assert transport["ssms"] == alpha_ssms_url
+    assert transport["proxy"]["server"] == "127.0.0.1"
+    assert transport["proxy"]["port"] == 49152
+
+
+def test_registration_actions(
+    client_library_session: ClientLibrary,
+    alpha_dev_ca,
+    alpha_ssms_url,
+    registration_token,
+):
+    """
+    Perform licensing actions:
+      (registration, re-registration, de-registration, auth renewal, reg renewal)
+    With valid certificate, transport settings and token.
+    """
+    cl = client_library_session
+
+    cl.licensing.install_certificate(alpha_dev_ca)
+    cl.licensing.set_transport(alpha_ssms_url)
+
+    # Register
+    cl.licensing.register_wait(token=registration_token)
+    status = cl.licensing.status()
+    assert status["registration"]["status"] == "COMPLETED"
+    assert status["authorization"]["status"] == "IN_COMPLIANCE"
+
+    # Re-register
+    cl.licensing.register_wait(token=registration_token, reregister=True)
+    status = cl.licensing.status()
+    assert status["registration"]["status"] == "COMPLETED"
+    assert status["authorization"]["status"] == "IN_COMPLIANCE"
+
+    # Renew authorization
+    auth_expiry = datetime.fromisoformat(
+        cl.licensing.status()["authorization"]["expires"]
+    )
+    cl.licensing.renew_authorization()
+    timeout = 20
+    for x in range(timeout):
+        auth_expiry_new = datetime.fromisoformat(
+            cl.licensing.status()["authorization"]["expires"]
+        )
+        if auth_expiry_new > auth_expiry:
+            break
+        else:
+            sleep(1)
+            continue
+    else:
+        pytest.fail(
+            f"Timed out waiting {timeout} secs for authorization renewal."
+        )
+    status = cl.licensing.status()
+    assert status["registration"]["status"] == "COMPLETED"
+    assert status["authorization"]["status"] == "IN_COMPLIANCE"
+
+    # Renew registration
+    reg_expiry = datetime.fromisoformat(
+        cl.licensing.status()["registration"]["expires"]
+    )
+    cl.licensing.register_renew()
+    timeout = 20
+    for x in range(timeout):
+        reg_expiry_new = datetime.fromisoformat(
+            cl.licensing.status()["registration"]["expires"]
+        )
+        if reg_expiry_new > reg_expiry:
+            break
+        else:
+            sleep(1)
+            continue
+    else:
+        pytest.fail(
+            f"Timed out waiting {timeout} secs for registration renewal."
+        )
+    status = cl.licensing.status()
+    assert status["registration"]["status"] == "COMPLETED"
+    assert status["authorization"]["status"] == "IN_COMPLIANCE"
+
+    # Deregister
+    cl.licensing.deregister()
+    status = cl.licensing.status()
+    assert status["registration"]["status"] == "NOT_REGISTERED"
+    assert status["authorization"]["status"] == "EVAL"
+
+
+# TODO: registration actions with invalid transport settings
+# TODO: registration actions with invalid token
+# TODO: min/max feature counts for every license type
+# TODO: SLR - enable, disable, request reservation, cancel request
+# TODO: SLR - mock server responses and test all functions

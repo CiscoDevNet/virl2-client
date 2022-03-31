0a21c5d53028d86f9ecf7bed526b1f3c425b416d
avirgovi <avirgovi@cisco.com>
2022-03-31 11:49:59 -0700

    remove integration tests from virl2_client repo/submodule
    delete conftest - not needed
    
    add missing fixture
    
    remove unused stuff from pytest.ini

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,6 +18,7 @@
 # limitations under the License.
 #
 
+import os
 import pytest
 import requests
 
@@ -63,3 +64,10 @@ def client_library_server_2_19_0():
 def mocked_session():
     with patch.object(requests, "Session", autospec=True) as session:
         yield session
+
+
+@pytest.fixture(scope="function")
+def change_test_dir(request):
+    os.chdir(request.fspath.dirname)
+    yield
+    os.chdir(request.config.invocation_dir)

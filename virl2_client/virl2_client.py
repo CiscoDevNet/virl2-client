0489e0485bb4c4c556050fce6e81c5ac9a0a9d1b
Tomas Mikuska <tmikuska@cisco.com>
2022-07-18 11:01:45 +0200

    Bump version to 2.4.1

--- virl2_client/virl2_client.py
+++ virl2_client/virl2_client.py
@@ -158,7 +158,7 @@ class ClientLibrary:
     """Python bindings for the REST API of a CML controller."""
 
     # current client version
-    VERSION = Version("2.4.0")
+    VERSION = Version("2.4.1")
     # list of Version objects
     INCOMPATIBLE_CONTROLLER_VERSIONS = [
         Version("2.0.0"),

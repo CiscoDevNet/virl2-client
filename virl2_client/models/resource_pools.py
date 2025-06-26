cff87484a1288edf1d4a31e42f9b70971915140f
Miroslav Los <mirlos@cisco.com>
2025-06-26 09:54:01 +0200

    SIMPLE-7993 - include original names for renamed modules

---
+++ virl2_client/models/resource_pools.py
@@ -0,0 +1,27 @@
+#
+# This file is part of VIRL 2
+# Copyright (c) 2019-2025, Cisco Systems, Inc.
+# All rights reserved.
+#
+# Python bindings for the Cisco VIRL 2 Network Simulation Platform
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import warnings
+
+from .resource_pool import *  # noqa
+
+warnings.warn(
+    "The module name 'virl2_client.models.resource_pools' is deprecated."
+    "Use 'virl2_client.models.resource_pool' instead.",
+)

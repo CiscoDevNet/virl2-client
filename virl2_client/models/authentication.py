962323e733d034ddbfdd268a53d0e5c36871f7ca
Tomas Mikuska <tmikuska@cisco.com>
2026-03-02 15:00:47 +0100

    Switch to APIError and add tests

--- virl2_client/models/authentication.py
+++ virl2_client/models/authentication.py
@@ -28,7 +28,7 @@ from uuid import uuid4
 
 import httpx
 
-from ..exceptions import APIError, InitializationError
+from ..exceptions import APIError
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -124,10 +124,12 @@ class TokenAuth(httpx.Auth):
             _LOGGER.warning("re-auth called on 401 unauthorized")
             self.token = None
             if not (self.client_library.username and self.client_library.password):
-                raise InitializationError(
+                raise APIError(
                     "JWT token expired and automatic re-authentication is not "
                     "possible because username/password are not configured. "
                     "Set client.jwtoken, or initialize with username/password.",
+                    request=response.request,
+                    response=response,
                 )
             request.headers["Authorization"] = f"Bearer {self.token}"
             response = yield request

8221a1513a018391651d03826c16048c35d95722
Tomas Mikuska <tmikuska@cisco.com>
2026-04-23 23:42:07 +0200

    Apply suggestions from code review
    Co-authored-by: Marek Paulik <marpauli@cisco.com>

--- examples/licensing.py
+++ examples/licensing.py
@@ -82,4 +82,4 @@ def main() -> int:
 
 
 if __name__ == "__main__":
-    raise SystemExit(main())
+    sys.exit(main())
--- examples/link_conditioning.py
+++ examples/link_conditioning.py
@@ -125,4 +125,4 @@ def main() -> int:
 
 
 if __name__ == "__main__":
-    raise SystemExit(main())
+    sys.exit(main())
